a5fafbbe5161acd1028445eef09d0b3240c12b5c
David Ferguson <deejai@live.com>
2022-10-16 18:40:28 -0600

    fixed exe

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
 import os
+import sys
 
-ROOT_DIR = f"{os.path.dirname(os.path.abspath(__file__))}/"
+ROOT_DIR = ""
+if getattr(sys, 'frozen', False):
+    ROOT_DIR = f"{os.path.dirname(sys.executable)}/"
+elif __file__:
+    ROOT_DIR = f"{os.path.dirname(os.path.abspath(__file__))}/"
 
 GAME_WIDTH = 640
 GAME_HEIGHT = 480
